d09b46eee70d4c307e6fdb49a74d9b65fb50fb79
Consuelo-Java <consuelogrechi@outlook.it>
2025-05-24 10:24:51 +0200

    Fase 2

---
+++ src/Documento.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Documento implements Serializable {
+    String titolo;
+    String autore;
+    String contenuto;
+
+    public Documento(String titolo, String autore, String contenuto) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.contenuto = contenuto;
+    }
+
+    public void salvaSuFile(String nomeFile){
+        File file = new File(nomeFile);
+        try {
+            OutputStream stream = new FileOutputStream(file);
+            ObjectOutputStream objectStream = new ObjectOutputStream(stream);
+            objectStream.writeObject(this);
+            System.out.println("Documento salvato su " + nomeFile);
+        } catch (IOException e) {
+            System.err.println("Si e' verificato un errore nella scrittura del file: " + e.getMessage());
+        }
+    }
+
+    public static Documento caricaDaFile(String nomeFile){
+        File file = new File(nomeFile);
+        try {
+            InputStream stream = new FileInputStream(file);
+            ObjectInputStream objectStream = new ObjectInputStream(stream);
+            return (Documento) objectStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Si e' verificato un errore nella lettura del file: " + e.getMessage());
+            return null;
+        }
+    }
+}
